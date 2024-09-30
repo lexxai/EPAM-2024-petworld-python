4dbe5453030144001d9ae3865dc6416efee4f8b0
lexxai <lexxai@gmail.com>
2024-09-30 16:28:48 +0300

    configure_logging

---
+++ pet-project/logging-config.py
@@ -0,0 +1,49 @@
+from logging.config import dictConfig
+
+
+def configure_logging():
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                    "format": "000%(name)s: %(lineno)d - %(message)s",
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                    "format": "000%(name)s: %(lineno)d - %(message)s",
+                },
+            },
+            "handlers": {
+                "default": {
+                    "class": "rich.logging.RichHandler",
+                    "level": "DEBUG",
+                    "formatter": "console",
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "pet.log",
+                    "maxBytes": 1024 * 1024 * 10,
+                    "backupCount": 3,
+                    "encoding": "utf8",
+                },
+            },
+            "loggers": {
+                "uvicorn": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "INFO",
+                },
+                "main": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "INFO",
+                    "propagate": False,
+                },
+            }, 
+        }
+    )
