05c7571c2b12bd1ef7e78abed02fc514f97b5d9a
lexxai <lexxai@gmail.com>
2024-10-02 04:13:47 +0300

    CORSMiddleware added

--- pet-project/main.py
+++ pet-project/main.py
@@ -3,11 +3,16 @@ from contextlib import asynccontextmanager
 from core.models import db_helper
 from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from logging_config import configure_logging
 from api import router as api_router
 from utils.measure_time import a_measure_time
 
+
+origins = [
+    "http://localhost:3333",
+]
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +30,14 @@ async def lifespan(app: FastAPI):
 main_app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
+)
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow only specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
 )
 
 main_app.include_router(
