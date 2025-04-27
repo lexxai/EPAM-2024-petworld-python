fb2f08b2fabadf8debc9457a9601cd037d31fb2b
LeX Xai <lexxai@users.noreply.github.com>
2025-04-27 13:03:45 +0300

    Update config.py
    :str

--- python/pet-project/core/config.py
+++ python/pet-project/core/config.py
@@ -9,7 +9,7 @@ from pydantic_settings import (
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
-    app_production_url = "https://stitmarathonlexxaiprod.z16.web.core.windows.net"
+    app_production_url: str = "https://stitmarathonlexxaiprod.z16.web.core.windows.net"
 
 
 class DatabaseConfig(BaseModel):
